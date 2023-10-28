2c6ae86b33019a86be2359f178d43a6bdf03fb22
komafuji820 <tfukae25@gmail.com>
2023-10-28 17:48:56 +0900

    エラーメッセージの部分テンプレート援用

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  
+  validates :nickname, :last_name, :first_name, :ruby_last_name, :ruby_first_name, :birthday, presence: true
+
 end
