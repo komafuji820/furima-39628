b9104f1e40c7f9a45c89319f0e2c9b58dd728997
komafuji820 <tfukae25@gmail.com>
2023-11-04 19:11:51 +0900

    出品データの保存

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -10,8 +10,8 @@ class ItemsController < ApplicationController
   end
 
   def create
-    item = Item.new(item_params)
-    if item.save
+    @item = Item.new(item_params)
+    if @item.save
       redirect_to root_path
     else
       render :new, status: :unprocessable_entity
@@ -20,6 +20,6 @@ class ItemsController < ApplicationController
 
   private
   def item_params
-    params.require(:item).permit(:name, :explain, :price).merge(user_id: current_user.id)
+    params.require(:item).permit(:name, :explain, :price, :image, :category_id, :condition_id, :cost_id, :prefecture_id, :span_id).merge(user_id: current_user.id)
   end
 end
