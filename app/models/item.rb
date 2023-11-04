b4185f2f639d1cd1123afc826f11e9c4b06c181f
komafuji820 <tfukae25@gmail.com>
2023-11-04 19:12:15 +0900

    バリデーションを設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,6 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  
   belongs_to :user
   belongs_to :category
   belongs_to :condition
@@ -6,5 +8,10 @@ class Item < ApplicationRecord
   belongs_to :prefecture
   belongs_to :span
   has_one_attached :image
+
+  validates :name, :explain, presence: true
+  validates :price, numericality: { in: (300..9999999), only_integer: true, message: "can't be blank" }
+  validates :category_id, :condition_id, :cost_id, :prefecture_id, :span_id, numericality: { other_than: 1, message: "can't be blank" }
+  validates :image, presence: true
 
 end
