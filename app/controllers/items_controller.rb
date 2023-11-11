8b78d18e6d4fd430c12010c4e17de955742d0dc3
komafuji820 <tfukae25@gmail.com>
2023-11-11 15:22:19 +0900

    編集ページへ遷移できるよう記述

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -19,11 +19,23 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
+    find_item
+  end
+
+  def edit
+    find_item
+    if current_user.id != @item.user_id
+      redirect_to root_path
+    end
   end
 
   private
   def item_params
     params.require(:item).permit(:name, :explain, :price, :image, :category_id, :condition_id, :cost_id, :prefecture_id, :span_id).merge(user_id: current_user.id)
   end
+
+  def find_item
+    @item = Item.find(params[:id])
+  end
+
 end
