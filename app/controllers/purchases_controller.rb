b22ce607a57ba7c26a7633808c0d2281a5a4bb18
komafuji820 <tfukae25@gmail.com>
2023-11-18 20:38:42 +0900

    購入ページの表示

---
+++ app/controllers/purchases_controller.rb
@@ -0,0 +1,10 @@
+class PurchasesController < ApplicationController
+  before_action :authenticate_user!
+
+  def index
+    @item = Item.find(params[:item_id])
+    if current_user.id == @item.user_id
+      redirect_to root_path
+    end
+  end
+end
