423ead00944c81b1df738f0929f088e9e61896db
komafuji820 <tfukae25@gmail.com>
2023-11-23 09:19:22 +0900

    郵便番号をFakerに置き換え

--- spec/factories/purchase_addresses.rb
+++ spec/factories/purchase_addresses.rb
@@ -1,11 +1,11 @@
 FactoryBot.define do
   factory :purchase_address do
-    post_code         {"123-4567"}
+    post_code         {Faker::Number.leading_zero_number(digits: 3) + '-' + Faker::Number.leading_zero_number(digits: 4)}
     prefecture_id     {Faker::Number.between(from: 2, to: 48)}
     municipalities    {Faker::Address.city}
     house_number      {Faker::Address.street_address}
     house_name        {Faker::Lorem.word}
-    phone_number      {Faker::Number.number(digits: 10)}
+    phone_number      {Faker::Number.number(digits: 11)}
     token             {Faker::Internet.device_token}
   end
 end
